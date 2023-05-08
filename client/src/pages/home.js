5a4f0d2cd4ddce0e80b77665dac71d68ecc333c4
saminchandeepa <saminchandeepa@gmail.com>
2023-05-08 18:04:31 +0530

    add the create new recipe page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 function Home() {
   //get al the recipes and map those all recipies
-  const addRecipies = () => {};
+  const alllRecipies = async () => {
+    await axios.get("http://localhost:3001/recipes/");
+  };
 
   return <div></div>;
 }
