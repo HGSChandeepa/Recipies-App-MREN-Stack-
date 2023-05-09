98371c5436b2c7305b081752c9964452bc99210a
saminchandeepa <saminchandeepa@gmail.com>
2023-05-09 13:09:05 +0530

    home page and also add the status code

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
+  const [allRecipesList, setAllRecipesList] = useState([]);
   //get al the recipes and map those all recipies
-  const alllRecipies = async () => {
-    await axios.get("http://localhost:3001/recipes/");
-  };
-
-  return <div></div>;
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const responce = await axios.get("http://localhost:3001/recipes/");
+        setAllRecipesList(responce.data);
+      } catch (err) {
+        console.err(err);
+      }
+    };
+    fetchRecipes();
+  }, []);
+  // console.log(allRecipesList);
+  return (
+    <div>
+      {allRecipesList.map((recipe) => {
+        <h1>{recipe.name}</h1>;
+      })}
+    </div>
+  );
 }
 
 export default Home;
